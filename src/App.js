165064c9cbd9adf7bb16a1c6788180cbfb85ba02
Amey Jeurkar <62188246+ahhmay@users.noreply.github.com>
2022-07-22 02:11:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MobileContainer from './components/MobileContainer/MobileContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <MobileContainer />
+    </div>
+  );
+}
+
+export default App;
